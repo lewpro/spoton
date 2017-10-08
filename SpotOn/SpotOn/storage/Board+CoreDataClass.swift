1c2ccff996c54e9e378f11d0e05063e954719b54
Adam Shepro <alshepro@gmail.com>
2017-10-08 13:39:13 -0400

    added board logic

--- SpotOn/SpotOn/storage/Board+CoreDataClass.swift
+++ SpotOn/SpotOn/storage/Board+CoreDataClass.swift
@@ -11,6 +11,15 @@ import CoreData
 
 @objc(Board)
 public class Board: NSManagedObject {
+    enum LetterState {
+        case ON
+        case OFF
+    }
+    
+    enum BoardExceptions: Error {
+        case invalidLetter
+    }
+    
     var score : Int16 {
         get {
             // TODO: Bitshift the lettermask over to calculate current score
@@ -18,12 +27,39 @@ public class Board: NSManagedObject {
         }
     }
     
-    func hasPoint(atIndex index: Int) -> Bool {
-        // TODO: Bitshift to find true / false value
-        return false
+    
+    func getIndexAt(letter l: String) -> Int? {
+        return Const.LetterSets.englishLetters.index(of: l)
     }
     
-    func togglePoint(atIndex index: Int) {
-        // Update bit at index
+    func resetBoard() {
+        self.letterMask = 0
     }
+    
+    private func togglePointAt(index i: Int) {
+        letterMask ^= 1 << i
+    }
+    
+    func togglePoint(_ l: String) throws {
+        if let i = getIndexAt(letter: l){
+            togglePointAt(index: i)
+        } else {
+            throw BoardExceptions.invalidLetter
+        }
+    }
+    
+    private func hasPointAt(index i: Int) -> Bool {
+        let bit = (letterMask >> i) & 1
+        return bit == 1
+    }
+    
+    func hasPointAt(Letter l: String) throws -> Bool {
+        if let i = getIndexAt(letter: l){
+            return hasPointAt(index: i)
+        } else {
+            throw BoardExceptions.invalidLetter
+        }
+    }
+    
 }
+
